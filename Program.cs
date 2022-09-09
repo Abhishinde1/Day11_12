0c7642a18bcf6e5de58596cb0fd3809e58d6b3bd
AbhiShinde1 <abhishinde572@gmail.com>
2022-09-09 20:31:15 +0530

    Json Uploaded

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace InventoryDataManagement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "C:\\Users\\Shree\\source\\InventoryDataManagement\\InventoryData.json";
+
+            fetchforjasonRiceData fetchforjasonRiceData = new fetchforjasonRiceData();  
+            Rice data=fetchforjasonRiceData.Read(filePath);
+           
+            //Console.WriteLine(data.typeofRice.Name);
+            //Console.WriteLine(data.typeofRice.price);
+            //Console.WriteLine(data.typeofRice.weight);
+            Console.WriteLine("data for Rice");
+
+            for(int i=0;i<data.typeofRice.Count;i++)
+            {
+                Console.WriteLine(data.typeofRice[i].Name);
+                Console.WriteLine(data.typeofRice[i].price);
+                Console.WriteLine(data.typeofRice[i].weight);
+                Console.WriteLine("-----------------");
+            }
+            Console.WriteLine("Data for Pulses");
+
+            for (int i = 0; i < data.typeofPulse.Count; i++)
+            {
+                Console.WriteLine(data.typeofPulse[i].Name);
+                Console.WriteLine(data.typeofPulse[i].price);
+                Console.WriteLine(data.typeofPulse[i].weight);
+                Console.WriteLine("-----------------");
+            }
+          
+            Console.WriteLine("Data for Wheat");
+
+            for (int i = 0; i < data.typeofWheat.Count; i++)
+            {
+                Console.WriteLine(data.typeofWheat[i].Name);
+                Console.WriteLine(data.typeofWheat[i].price);
+                Console.WriteLine(data.typeofWheat[i].weight);
+                Console.WriteLine("-----------------");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ fetchforjasonRiceData.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryDataManagement
+{
+    public class fetchforjasonRiceData
+    {
+        public Rice Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Rice>(json);
+                }
+                catch(Exception)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
